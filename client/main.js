142c23882ffebdca04a1a523cf637aa4f26c6ee1
SiriusLL <sirius.love.light@gmail.com>
2021-06-07 23:03:10 -0600

    login logic render pet

--- client/main.js
+++ client/main.js
@@ -1,14 +1,23 @@
 Moralis.initialize('m20PA3clspCzNMiWXM5okTJK6gUbHgxPya6UFVmt'); // Application id from moralis.io
 Moralis.serverURL = 'https://rhdnu7cf8pia.moralis.io:2053/server'; //Server url from moralis.io
 
-async function login() {
+async function init() {
   try {
-    user = await Moralis.Web3.authenticate();
-    console.log(user);
-    alert('User logged in');
+    let user = Moralis.User.current();
+    if (!user) {
+      $('login_button').click(async () => {
+        user = await Moralis.Web3.authenticate();
+      });
+    }
+    renderGame();
   } catch (error) {
     console.log(error);
   }
 }
 
-document.getElementById('login_button').onclick = login;
+function renderGame() {
+  $('#game').show();
+  $('#login_button').hide();
+}
+
+init();
